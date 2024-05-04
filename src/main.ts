644a2bf7972c7a2ddb4cb6c7226d7d77185a3f9b
Jasper2002 <46618765+jasper1902@users.noreply.github.com>
2024-05-04 15:45:55 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.use(cookieParser);
+  app.use(cookieParser()); 
   await app.listen(3000);
 }
 bootstrap();
